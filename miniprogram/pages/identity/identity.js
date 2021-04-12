83b3dce89a1a59a91ef702383d8e9ad0298d209b
peng <qwe@qq.com>
2021-04-12 18:14:59 +0800

    feat: bishe

---
+++ miniprogram/pages/identity/identity.js
@@ -0,0 +1,60 @@
+Page({
+  data: {
+    type: '',
+    username: '',
+    password: ''
+  },
+
+  getUsr(event) {
+    this.setData({
+      username: event.detail,
+    });
+  },
+
+  getPwd(event) {
+    this.setData({
+      password: event.detail,
+    });
+  },
+  
+  getType(event) {
+    this.setData({
+      type: event.detail,
+    });
+  },
+
+  onLogin() {
+    let username = this.data.username
+    let password = this.data.password
+    let type = this.data.type
+
+    wx.cloud.database().collection('user').where({
+      username: '111'
+    }).get({
+      success(res) {
+        let usr = res
+        console.log(usr)
+        if (password == usr.password) {
+          // wx.showToast({
+          //   title: '登录成功',
+          // })
+          // wx.navigateTo({
+          //   url: '/pages/homepage/homepage',
+          // })
+          //wx.setStorageSync('usr', usr)
+          console.log("success")
+        }
+        else {
+          wx.showToast({
+            title: '登录失败',
+          })
+          console.log("fail")
+        }
+      },
+      fail(res) {
+        console.log("获取数据失败", res)
+      }
+    })
+    
+  }
+});
